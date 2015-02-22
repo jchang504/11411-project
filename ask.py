5b992491c743098d22f0446c0c748cbf5bde47a7
Jemmin Chang <jemminchang@cmu.edu>
2015-02-22 18:00:03 -0500

    Localized SIMPLE_PREDICATE to ask

--- ask.py
+++ ask.py
@@ -8,7 +8,9 @@ num_questions = sys.argv[2]
 sentence_trees = preprocess.parseHTML(article_filename)
 # ??? - Eileen's part
 # extracted_phrases = eileenspart(sentence_trees)
-extracted_phrases = [(transform.SIMPLE_PREDICATE, sentence_trees[0])] # for testing
+# TODO: fix this
+SIMPLE_PREDICATE = 0
+extracted_phrases = [(SIMPLE_PREDICATE, sentence_trees[0])] # for testing
 questions = transform.make_question(extracted_phrases)
 for q in questions:
   print q
