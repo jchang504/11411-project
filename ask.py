aabf6d5a5531429f3f98f1ab2e69252133893158
Jemmin Chang <jemminchang@cmu.edu>
2015-02-22 18:04:56 -0500

    Hacks

--- ask.py
+++ ask.py
@@ -10,7 +10,7 @@ sentence_trees = preprocess.parseHTML(article_filename)
 # extracted_phrases = eileenspart(sentence_trees)
 # TODO: fix this
 SIMPLE_PREDICATE = 0
-extracted_phrases = [(SIMPLE_PREDICATE, sentence_trees[0])] # for testing
+extracted_phrases = [(SIMPLE_PREDICATE, sentence_trees[0][0])] # for testing
 questions = transform.make_questions(extracted_phrases)
 for q in questions:
   print q
