d6ca7ccad4601a312b41dccf1c3b950668befb2b
Jemmin Chang <jemminchang@cmu.edu>
2015-02-22 18:01:36 -0500

    It's questions, not question

--- ask.py
+++ ask.py
@@ -11,6 +11,6 @@ sentence_trees = preprocess.parseHTML(article_filename)
 # TODO: fix this
 SIMPLE_PREDICATE = 0
 extracted_phrases = [(SIMPLE_PREDICATE, sentence_trees[0])] # for testing
-questions = transform.make_question(extracted_phrases)
+questions = transform.make_questions(extracted_phrases)
 for q in questions:
   print q
