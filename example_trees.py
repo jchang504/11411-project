e7b3562b0a92f6da3f749facb77efb8f12cb5785
Jemmin Chang <jemminchang@cmu.edu>
2015-04-12 17:54:16 -0400

    Implemented bin_q_to_sent
    sent_transform.py functions tested, working

--- example_trees.py
+++ example_trees.py
@@ -1,4 +1,10 @@
 from nltk.tree import Tree
 
+# John ate a burrito in the park.
 T1 = Tree.fromstring('(ROOT (S (NP (NNP John)) (VP (VBD ate) (NP (DT a) (NN burrito)) (PP (IN in) (NP (DT the) (NN park)))) (. .)))')
+# John, a doctor, treated him.
 T2 = Tree.fromstring('(ROOT (S (NP (NP (NNP John)) (, ,) (NP (DT a) (NN doctor)) (, ,)) (VP (VBD treated) (NP (PRP him))) (. .)))')
+# Did John eat a burrito in the park?
+S1 = Tree.fromstring('(ROOT (SQ (VBD Did) (NP (NNP John)) (VP (VB eat) (NP (NP (DT a) (NN burrito)) (PP (IN in) (NP (DT the) (NN park))))) (. ?)))')
+# Is John a doctor?
+S2 = Tree.fromstring('(ROOT (SQ (VBZ Is) (NP (NNP John)) (NP (DT a) (NN doctor)) (. ?)))')
--- sent_transform.py
+++ sent_transform.py
@@ -4,10 +4,17 @@ from nltk.tree import Tree
 from tags import *
 from pattern.en import conjugate, lemma
 
-# identifies the question as either WH or BINARY
-# question_tree is a Tree
+WH = 'wh'
+BINARY = 'binary'
+# identifies the question as either WH or BINARY, or returns None if unsure
+# question_tree is a Tree (directly descended from ROOT)
 def q_type(question_tree):
-  pass # TODO: implement
+  if question_tree.label() == WH_QUESTION:
+    return WH
+  elif question_tree.label() == BIN_QUESTION:
+    return BINARY
+  else:
+    return None
 
 # inverts the subj and aux (do-insertion if necessary) to transform the
 # declarative sentence into a binary interrogative form
@@ -15,31 +22,54 @@ def q_type(question_tree):
 # RETURNS: a string. The original subj is uncapitalized (if non-proper) but the
 # aux is NOT capitalized.
 def sent_to_bin_q(sentence_tree):
-  np = sentence_tree[0]
+  assert(sentence_tree.label() == SENTENCE)
+  subj = sentence_tree[0]
+  assert(subj.label() == NP)
   vp = sentence_tree[1]
+  assert(vp.label() == VP)
   head_verb = vp[0]
   assert(is_verb(head_verb.label()))
   verb = head_verb[0]
 
   # has auxiliary
   if is_modal(head_verb, vp) or lemma(verb) == 'be':
-    uncap(np) # uncapitalize original subj (unless proper noun)
-    return verb + ' ' + tree_to_string(np) + ' ' + tree_to_string(vp[1])
+    uncap(subj) # uncapitalize original subj (unless proper noun)
+    return ' '.join([verb, tree_to_string(subj), tree_to_string(vp[1])])
 
   # no auxiliary, use do-insertion
   else:
     head_verb[0] = lemma(verb) # convert head verb to infinitive
-    uncap(np) # uncapitalize original subj (unless proper noun)
-    return (conjugate('do', get_inflection(head_verb)) + ' ' +
-        tree_to_string(np) + ' ' + tree_to_string(vp))
+    uncap(subj) # uncapitalize original subj (unless proper noun)
+    return ' '.join([conjugate('do', get_inflection(head_verb)),
+        tree_to_string(subj), tree_to_string(vp)])
 
 # inverts the aux (do-deletion if necessary) and subj to transform the binary
 # interrogative into a declarative sentence
-# REQUIRES: question_tree is a Tree
+# REQUIRES: question_tree is a Tree (directly descended from ROOT)
 # RETURNS: a string. The original aux is uncapitalized and the subj is
 # capitalized
 def bin_q_to_sent(question_tree):
-  pass # TODO: implement
+  assert(question_tree.label() == BIN_QUESTION)
+  aux = question_tree[0]
+  uncap(aux) # uncapitalize the aux
+  assert(is_verb(aux.label()))
+  subj = question_tree[1]
+  assert(subj.label() == NP)
+  pred = question_tree[2]
+  assert(pred.label() == VP or pred.label() == NP)
+
+  # do-deletion
+  if lemma(aux[0]) == 'do':
+    assert(is_verb(pred.label()))
+    head_verb = pred[0]
+    # re-conjugate head verb with inflection of aux 'do'
+    head_verb[0] = conjugate(head_verb[0], get_inflection(aux))
+    return ' '.join([tree_to_string(subj), tree_to_string(pred)])
+
+  # real (non-do) auxiliary
+  else:
+    return ' '.join([tree_to_string(subj).capitalize(), aux[0],
+        tree_to_string(pred)])
 
 # transforms the apposition 'np1, np2,' into a declarative sentence
 def apposition_to_sent(np1, np2):
