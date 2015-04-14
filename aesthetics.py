36f42ff2c9ce65818b10d3b2052cf51282a16ff4
Jemmin Chang <jemminchang@cmu.edu>
2015-04-14 18:29:16 -0400

    Implemented aesthetics

--- aesthetics.py
+++ aesthetics.py
@@ -1,2 +1,13 @@
-# add/remove punctuation
-# change -LRB- and -RRB- to ( and )
+import re
+
+# make the sentence look like a proper declarative or question, adding/removing
+# punctuation, replacing those ugly -LRB- and -RRB-, and capitalizing the
+# sentence
+def make_pretty(sentence, end_punctuation):
+  sentence.replace('-LRB- ', '(')
+  sentence.replace(' -RRB-', ')')
+  sentence = re.sub(r' *([,;:\.\?!])', r'\1', sentence)
+  m = re.match(r'([^\.\?!]+)[\.\?!]*', sentence)
+  sentence = m.group(1)
+  sentence = sentence[0].upper() + sentence[1:]
+  return sentence + end_punctuation
